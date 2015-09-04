8e38be2d433c1b1a0f2e4b0339ed9250af7492e0
Nico Watine <contact@studioncreations.com>
2015-09-04 13:40:43 -0500

    /print-work/ Conversion DONE

--- print-work/houston-oculofacial-spring-postcard.php
+++ print-work/houston-oculofacial-spring-postcard.php
@@ -1,25 +1,24 @@
+<?php 
+	include($_SERVER['DOCUMENT_ROOT'].'/php/autoVer.php');
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html class="no-js" lang="en">
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-	<link type="text/plain" rel="author" href="/humans.txt" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
 	<title>Promotional Spring Postcard for Houston Oculofacial Plastic Surgery | Studio N Creations</title>
 	<meta name="description" content="Keep up with the latest projects and happenings here at the Studio N Creations blog" />
-	<link rel="shortcut icon" href="/favicon.ico" />
-	<link rel="icon" sizes="192x192" href="/touch-icon-192x192.png" />
-
-	<link rel="stylesheet" href="/css/style.css" />
+	<link type="text/plain" rel="author" href="/humans.txt">
+	
+	<link rel="stylesheet" href="<?php autoVer('/css/style.css'); ?>"/>
 
 	<!--[if lt IE 9]>
+		<link rel="stylesheet" href="<?php autoVer('/css/ie.css'); ?>"/>
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	<![endif]-->
-
-	<!--<script>
-		google-analytics
-	</script>-->
 </head>
-
 
 <body>
 <header class="top-bar">
@@ -31,9 +30,8 @@
 			<nav class="nav-collapse">
 				<ul>
 					<li><a href="/">Home</a></li>
-					<li><a href="http://studioncreations.com/contact.html">Categories</a></li>
-					<li><a href="/tags">Tags</a></li>
-					<li class="active"><a href="/">Blog</a></li>
+					<li><a href="/categories/">Categories</a></li>
+					<li><a href="/tags/">Tags</a></li>
 				</ul>
 			</nav>
 		</div>
@@ -50,13 +48,13 @@
 				<h3 id="day">1</h3>
 				<h3 id="month">apr</h3>
 			</div>
-			<h5><a href="/print-work/">print-work</a> | <a href="/tags/houston-oculofacial.html">houston-oculofacial</a></h5>
+			<h5><a href="/print-work/">print-work</a> | <a href="/tags/houston-oculofacial">houston-oculofacial</a></h5>
 			<h5>
 		     
-		        <a href="/web-design/greencare-skeleton-setup.html" title="Previous Post: Greencare Site Structure Setup">&laquo; Previous</a> -
+		        <a href="/web-design/greencare-skeleton-setup" title="Previous Post: Greencare Site Structure Setup">&laquo; Previous</a> -
 		    
 		     
-		            <a href="/web-design/lemon-peel-facial-logo.html" title="next Post: One of Our Favorite Logos">Next &raquo; </a> 
+		            <a href="/web-design/lemon-peel-facial-logo" title="next Post: One of Our Favorite Logos">Next &raquo; </a> 
 		    
 		    </h5>
 		</div>
@@ -84,12 +82,11 @@
 </main>
 
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<script src="/js/scripts.min.js"></script>
-<script src="/js/src/rem.min.js"></script>
+<script src="<?php autoVer('/js/scripts-min.js'); ?>"></script>
 
- 
+<!--[if lt IE 9]>
+	<script src="<?php autoVer('/js/ie-min.js'); ?>"></script>
+<![endif]-->
 
 </body>
 </html>
-
--- print-work/jardin-de-france-tag-examples.php
+++ print-work/jardin-de-france-tag-examples.php
@@ -1,25 +1,24 @@
+<?php 
+	include($_SERVER['DOCUMENT_ROOT'].'/php/autoVer.php');
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html class="no-js" lang="en">
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-	<link type="text/plain" rel="author" href="/humans.txt" />
-	<title>The Jardin de France Tags - Examples | Studio N Creations</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<title>New Print Material for 'Jardin de France' | Studio N Creations</title>
 	<meta name="description" content="Keep up with the latest projects and happenings here at the Studio N Creations blog" />
-	<link rel="shortcut icon" href="/favicon.ico" />
-	<link rel="icon" sizes="192x192" href="/touch-icon-192x192.png" />
-
-	<link rel="stylesheet" href="/css/style.css" />
+	<link type="text/plain" rel="author" href="/humans.txt">
+	
+	<link rel="stylesheet" href="<?php autoVer('/css/style.css'); ?>"/>
 
 	<!--[if lt IE 9]>
+		<link rel="stylesheet" href="<?php autoVer('/css/ie.css'); ?>"/>
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	<![endif]-->
-
-	<!--<script>
-		google-analytics
-	</script>-->
 </head>
-
 
 <body>
 <header class="top-bar">
@@ -31,9 +30,8 @@
 			<nav class="nav-collapse">
 				<ul>
 					<li><a href="/">Home</a></li>
-					<li><a href="http://studioncreations.com/contact.html">Categories</a></li>
-					<li><a href="/tags">Tags</a></li>
-					<li class="active"><a href="/">Blog</a></li>
+					<li><a href="/categories/">Categories</a></li>
+					<li><a href="/tags/">Tags</a></li>
 				</ul>
 			</nav>
 		</div>
@@ -50,13 +48,13 @@
 				<h3 id="day">30</h3>
 				<h3 id="month">mar</h3>
 			</div>
-			<h5><a href="/print-work/">print-work</a> | <a href="/tags/jardin-de-france.html">jardin-de-france</a></h5>
+			<h5><a href="/print-work/">print-work</a> | <a href="/tags/jardin-de-france">jardin-de-france</a></h5>
 			<h5>
 		     
-		        <a href="/web-design/dr-esposito-wrap-up.html" title="Previous Post: Project Wrapped Up! lisaesposito.com">&laquo; Previous</a> -
+		        <a href="/web-design/dr-esposito-wrap-up" title="Previous Post: Project Wrapped Up! lisaesposito.com">&laquo; Previous</a> -
 		    
 		     
-		            <a href="/web-design/greencare-skeleton-setup.html" title="next Post: Greencare Site Structure Setup">Next &raquo; </a> 
+		            <a href="/web-design/greencare-skeleton-setup" title="next Post: Greencare Site Structure Setup">Next &raquo; </a> 
 		    
 		    </h5>
 		</div>
@@ -96,12 +94,11 @@
 </main>
 
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<script src="/js/scripts.min.js"></script>
-<script src="/js/src/rem.min.js"></script>
+<script src="<?php autoVer('/js/scripts-min.js'); ?>"></script>
 
- 
+<!--[if lt IE 9]>
+	<script src="<?php autoVer('/js/ie-min.js'); ?>"></script>
+<![endif]-->
 
 </body>
 </html>
-
