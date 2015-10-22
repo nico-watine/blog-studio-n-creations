20fc9f14989abdf9e7c29617eb70e7497a155e5a
Nico Watine <contact@studioncreations.com>
2015-10-22 10:57:24 -0500

    Created new 'blog.css' and linked to it
    This file will be autoversioned solely by the blog

--- _includes/components/head.php
+++ _includes/components/head.php
@@ -6,17 +6,18 @@
 <head>
 	<meta charset="utf-8">
 	<link rel="preconnect" href="http://cdn.studioncreations.com">
-	<title>{{ page.title }} | Studio N Creations</title>
+	<title>{{ page.title }} | Studio N Creations Blog</title>
 	<meta name="description" content="Keep up with the latest projects and happenings here at the Studio N Creations blog" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link type="text/plain" rel="author" href="/humans.txt">
 	
 	<link rel="stylesheet" href="http://cdn.studioncreations.com/css/core.css" />
-	<link rel="stylesheet" href="/css/style.css" />
+	<link rel="stylesheet" href="<?php autoVer('/css/blog.css'); ?>" />
 
 	<script src="http://cdn.studioncreations.com/js/modernizr-min.js"></script>
 
 	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<link rel="stylesheet" href="http://cdn.studioncreations.com/css/ie.css" />
+		<script src="http://cdn.studioncreations.com/js/ie-min.js"></script>
 	<![endif]-->
 </head>
--- _site/index.php
+++ _site/index.php
@@ -6,18 +6,19 @@
 <head>
 	<meta charset="utf-8">
 	<link rel="preconnect" href="http://cdn.studioncreations.com">
-	<title>Blog Roll | Studio N Creations</title>
+	<title>Blog Roll | Studio N Creations Blog</title>
 	<meta name="description" content="Keep up with the latest projects and happenings here at the Studio N Creations blog" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link type="text/plain" rel="author" href="/humans.txt">
 	
 	<link rel="stylesheet" href="http://cdn.studioncreations.com/css/core.css" />
-	<link rel="stylesheet" href="/css/style.css" />
+	<link rel="stylesheet" href="<?php autoVer('/css/blog.css'); ?>" />
 
 	<script src="http://cdn.studioncreations.com/js/modernizr-min.js"></script>
 
 	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<link rel="stylesheet" href="http://cdn.studioncreations.com/css/ie.css" />
+		<script src="http://cdn.studioncreations.com/js/ie-min.js"></script>
 	<![endif]-->
 </head>
 
