f344073548345c7337a4bb4c9e4be434ec69c8fc
Nico Watine <contact@studioncreations.com>
2015-12-07 14:17:13 -0600

    CATEGORY template folder cleaned up

--- _layouts/category/projects.php
+++ _layouts/category/projects.php
@@ -1,48 +1,22 @@
-{% include components/head.php %}
+{% include components/head-og.php %}
 
-<body>
+<body class="blog-index">
 {% include blocks/top-bar.html %}
+{% include blocks/nav.html %}
 
 <main class="outer-container">
 	<section class="blog-roll">
 		{% for post in site.categories.projects %}
-			<article class="post-link">
-				<header class="row" id="post-details">
-					<div class="post-date"> 
-						<h3 id="day">{{ post.day }}</h3>
-						<h3 id="month">{{ post.month }}</h3>
-					</div>
-					<div class="post-head">
-						<a href="/{{ post.category-link }}/{{ post.slug }}" class="index-page-post-link">
-							<h2 class="link-title">{{ post.title }}</h2>
-						</a>	
-						<h4 class="link-subtitle">{{ post.excerpt }}</h4>
-					</div>
-				</header>
-				<div class="row" id="img-row">
-					<a href="/{{ post.category-link }}/{{ post.slug }}"> 
-						<img src="{{ site.blog_cdn }}{{ post.link-image }}" alt="">
-					</a>
-				</div>
-				<div class="row" id="post-taxonomy">
-					<h6>Filed Under: <a href="/{{ post.category }}/">{{ post.category }}</a></h6>
-					<h6>Tags: <a href="/tags/{{ post.tags }}">{{ post.tags }}</a></h6>
-				</div>
-			</article>
+			{% include blocks/article/article-post-summary.html %}
 		{% endfor %}
 	</section>
 	<aside class="sidebar">
 		{% include blocks/side-bar/category-title.html %}
-		{% include blocks/side-bar/category-widget.html %}
+		{% include blocks/side-bar/category-nav.html %}
 	</aside>
 </main>
 
 {% include blocks/footer.html %}
-
-{% include blocks/nav.html %}
-
 {% include components/scripts/scripts.php %}
- 
-
 </body>
 </html> 
