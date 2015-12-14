ff718c6e87a8665d614dfa2df8e509452a7502ec
Nico Watine <contact@studioncreations.com>
2015-12-14 10:54:55 -0600

    Cleaning up of /js (renamed article-scripts to article.js) (deleted js/rem.min.js bc was not imported by anything)

--- _includes/components/scripts/article-scripts.php
+++ _includes/components/scripts/article-scripts.php
@@ -1,2 +1,2 @@
-<script src="{{ site.blog_cdn }}<?php autoVer('/js/article-scripts-min.js'); ?>"></script>
+<script src="{{ site.blog_cdn }}<?php autoVer('/js/article-min.js'); ?>"></script>
 <script async defer data-pin-hover="true" data-pin-color="white" src="//assets.pinterest.com/js/pinit.js"></script>
